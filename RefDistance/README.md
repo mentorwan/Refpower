70869a44d7d74a68048e4769fe14d6424302e123
Yunhu Wan <yunhuw@gmail.com>
2020-03-04 14:14:53 -0500

    changes syncing with draft

--- RefDistance/README.md
+++ RefDistance/README.md
@@ -1,90 +1,24 @@
-# RefDistance (Within HMP samples)
+# RefDistance
+#
+# #######################################################################
+# Dr. Yunhu Wan 
+# Dr. Mitch Gail
+# Dr. Jianxin Shi
+# Mar 4, 2020                          
+# The purpose of this script is to generate BC and Unifrac distances to 
+# HMP 92 reference stool samples and HMP 74 reference nasal samples.
+# ######################################################################
 
-This function will generate Bray Curtis and Unweighted/Weighted Unifrac distance to HMP reference stool sample and HMP reference nasal samples
-
-Example code is in RefDistance.R file.
-
-## Bray_Curtis Distance
-
-Input: 
-tax.level.bc #  taxonomy level, one of the following: "l2.phylum", "l3.class", "l4.order", "l5.family", "l6.genus"
-BC_file_name # file name for relative abundance file in given taxnomy level
-
-Output:
-hmp.dist.bc  # n*2 matrix
-```
-tax.level.bc = "l2.phylum"
-BC_file_name = "HMP_L2.txt"
-
-rel_file <- read.table(BC_file_name,check.names=F,header = T)
-hmp.dist.bc <- HMPdistance(tax.level.bc, d.new.filename = BC_file_name, d.new.ix.col.not.rel.abu = 1:7, measure = 'bc')
-rownames(hmp.dist.bc) <- rel_file[,1]
-
-```
-## Unweighted Unifrac Distance
+Usage 1 (HMP samples): Rscript RefDistance.R HMP_L2.txt HMP_YW_even1000.biom L2 Y output.txt
+Usage 2 (non HMP samples): Rscript RefDistance.R AG_1879_L2.txt AG_1879_final.biom L2 N output.txt
 
 Input: 
-tax.level.unifrac #  taxonomy level depending on biom file. In this case. "Rank2","Rank3","Rank4","Rank5","Rank6"
-data  #  original biom file 
-tree # phylogentic tree.
 
-Output:
-hmp.dist.uw  # n*2 matrix
+Relative abundance file (HMP_L2.txt)
+sample data biom file (HMP_YW_even1000.biom)
+tax.level (L2)
+Y/N  (test samples are HMP sample or non-HMP samples)
 
+Output: (output.txt)
 
-```
-tax.level.unifrac = "Rank2"
-data = "HMP_YW_even1000.biom"
-treefile = "rep_set_v35.tre"
-unifrac_uw = Calculate_Unifrac(data,tax.level.unifrac,treefile,"uw")
-sample_list <- rownames(unifrac_uw)
-dist.to.stool <- hmp.distance.helper.new(d.ref = as.character(d.ref.stool.info$sample.id), 
-                                         d.new = sample_list,
-                                         measure = "uw")
-
-dist.to.nasal <- hmp.distance.helper.new(d.ref = as.character(d.ref.nasal.info$sample.id), # matrix of rel abu
-                                         d.new = sample_list,
-                                         measure = "uw")
-hmp.dist.uw <- cbind(dist.to.stool, dist.to.nasal)
-
-```
-
-## Weighted Unifrac Distance
-
-Input: 
-tax.level.unifrac #  taxonomy level depending on biom file. In this case. "Rank2","Rank3","Rank4","Rank5","Rank6"
-data  #  original biom file 
-tree # phylogentic tree.
-
-Output:
-hmp.dist.wu  # n*2 matrix
-
-
-```
-tax.level = "Rank2"
-data = "HMP_YW_even1000.biom"
-treefile = "rep_set_v35.tre"
-unifrac_uw = Calculate_Unifrac(data,tax.level.unifrac,treefile,"uw")
-sample_list <- rownames(unifrac_uw)
-dist.to.stool <- hmp.distance.helper.new(d.ref = as.character(d.ref.stool.info$sample.id), 
-                                         d.new = sample_list,
-                                         measure = "wu")
-
-dist.to.nasal <- hmp.distance.helper.new(d.ref = as.character(d.ref.nasal.info$sample.id), 
-                                         d.new = sample_list,
-                                         measure = "wu")
-hmp.dist.wu <- cbind(dist.to.stool, dist.to.nasal)
-
-```
-
-# RefDistance_AG (AG samples against HMP reference and stool samples)
-
-Input: 
-tax.level
-Relative abundance file.
-Tree file
-sample data biom file
-full data with HMP samples 
-
-Output:
 A table with matrix n*6 which n is number of samples. For each distance, it includes 2 distances to HMP stool and HMP nasal.
--- RefDistance/RefDistance.R
+++ RefDistance/RefDistance.R
@@ -1,34 +1,86 @@
 
 #######################################################################
-# Yunhu Wan 
-# Aug 6, 2019                          
-# The purpose of this script is to generate BC and Unifrac distance to 
-# HMP reference stool and HMP reference nasal samples.
+# Dr. Yunhu Wan 
+# Dr. Mitch Gail
+# Mar 4, 2020                          
+# The purpose of this script is to generate BC and Unifrac distances to 
+# HMP 92 reference stool samples and HMP 74 reference nasal samples.
 #######################################################################
 
-setwd("~/Desktop/Mitch_Power/Dropbox/Code/standard_aug2019/Deposiit/") # Working folder.Change to directory it needs.
+setwd(".") # Working folder. Change to directory it needs.
 rm(list=ls())
+
+# Notes: The phyloseq package needs to be installed
 
 source('microbiome-fixed-reference-functions.r')
 
 load("taxonomy-GG13-8-and-HMP-reference-sets-L2-L6.rdata")  #Reference Stool and Nasal samples
 
 
+args = commandArgs(trailingOnly = TRUE)
+
+
+BC_input = args[1]
+Unifrac_input = args[2]
+level = args[3]
+HMP_tag = args[4]
+Output_file = args[5]
+
 #For Bray Curtis, it needs Relative abundance table for each taxonomy level
 
-tax.level.bc = "l2.phylum" #"l2.phylum", "l3.class", "l4.order", "l5.family","l6.genus"
-BC_file_name = "HMP_L2.txt" #phylum: HMP_L2.txt, class: HMP_L3.txt, order: HMP_L4.txt, family: HMP_L5.txt, genus: HMP_L6.txt
+#level is one of "l2.phylum", "l3.class", "l4.order", "l5.family","l6.genus"
+if(level == 'L2' || level == "l2")
+{
+  tax.level.bc = "l2.phylum" 
+  tax.level.unifrac="Rank2"
+} else if (level == 'L3' || level == "l3")
+{
+  tax.level.bc = "l3.class"
+  tax.level.unifrac="Rank3"
+}else if (level == 'L4' || level == "l4")
+{
+  tax.level.bc = "l4.order"
+  tax.level.unifrac="Rank4"
+}else if (level == 'L5' || level == "l5")
+{
+  tax.level.bc = "l5.family"
+  tax.level.unifrac="Rank5"
+}else if (level == 'L6' || level == "l6")
+{
+  tax.level.bc = "l6.genus"
+  tax.level.unifrac = "Rank6"
+}
+
+#BC_file_name = "HMP_L2.txt" #phylum: HMP_L2.txt, class: HMP_L3.txt, order: HMP_L4.txt, family: HMP_L5.txt, genus: HMP_L6.txt
+BC_file_name = BC_input
 
 rel_file <- read.table(BC_file_name,check.names=F,header = T)
-hmp.dist.bc <- HMPdistance(tax.level.bc, d.new.filename = BC_file_name, d.new.ix.col.not.rel.abu = 1:7, measure = 'bc')
+
+if(HMP_tag == "Y")   #GG 2011 database
+{
+  hmp.dist.bc <- HMPdistance(tax.level.bc, d.new.filename = BC_file_name, d.new.ix.col.not.rel.abu = 1:7, measure = 'bc')
+}else if (HMP_tag == "N") ## GG 13_8 database
+{
+  hmp.dist.bc <- HMPdistance(tax.level.bc, d.new.filename = BC_file_name, d.new.ix.col.not.rel.abu = 1:14, measure = 'bc')
+}
+
 rownames(hmp.dist.bc) <- rel_file[,1]
 
 
 #Unweighted Unifrac 
 
-tax.level.unifrac = "Rank2" #phylum: "Rank2", class: "Rank3", order: "Rank4", family: "Rank5", genus: "Rank6"
-data = "HMP_YW_even1000.biom"
-treefile = "rep_set_v35.tre"
+#tax.level.unifrac = "Rank2" #phylum: "Rank2", class: "Rank3", order: "Rank4", family: "Rank5", genus: "Rank6"
+#data = "HMP_YW_even1000.biom"
+data = args[2]
+
+if(HMP_tag == "Y")   #GG 2011 database
+{
+  treefile = "rep_set_v35.tre"
+}else if (HMP_tag == "N") ## GG 13_8 database
+{
+  treefile = "97_otus.tree"
+}
+
 unifrac_uw = Calculate_Unifrac(data,tax.level.unifrac,treefile,"uw")
 sample_list <- rownames(unifrac_uw)
 dist.to.stool <- hmp.distance.helper.new(d.ref = as.character(d.ref.stool.info$sample.id), # matrix of rel abu
@@ -44,9 +96,9 @@ rownames(hmp.dist.uw) <- as.integer(sample_list)
 
 
 #Weighted Unifrac
-tax.level.unifrac = "Rank2" #phylum: "Rank2", class: "Rank3", order: "Rank4", family: "Rank5", genus: "Rank6"
-data = "HMP_YW_even1000.biom"
-treefile = "rep_set_v35.tre"
+#tax.level.unifrac = "Rank2" #phylum: "Rank2", class: "Rank3", order: "Rank4", family: "Rank5", genus: "Rank6"
+#data = "HMP_YW_even1000.biom"
+#treefile = "rep_set_v35.tre"
 unifrac_wu = Calculate_Unifrac(data,tax.level.unifrac,treefile,"wu")
 sample_list <- rownames(unifrac_wu)
 dist.to.stool <- hmp.distance.helper.new(d.ref = as.character(d.ref.stool.info$sample.id), # matrix of rel abu
@@ -62,7 +114,7 @@ rownames(hmp.dist.uw) <- as.integer(sample_list)
 
 All_matrix <- cbind(hmp.dist.bc,hmp.dist.uw,hmp.dist.wu)
 
-write.table(All_matrix,"output.txt",quote=FALSE,sep="\t")
+write.table(All_matrix,Output_file,quote=FALSE,sep="\t")
 
 
 
